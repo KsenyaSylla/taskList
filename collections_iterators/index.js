53dfa90cf4bf6fb0717530694f3352f2b748faba
Sylla Ksenia <natoire@yandex.ru>
2024-01-09 12:58:54 +0300

    classwork

---
+++ collections_iterators/index.js
@@ -0,0 +1,27 @@
+/*Создать механизм для безопасного добавления метаданных к
+объектам книг с использованием Symbol.
+*/
+//1. Создать уникальные символы для метаданных: отзывы, рейтинг, теги.
+
+const comment = Symbol("comment");
+const rate = Symbol("rate");
+const tag = Symbol("rate");
+
+// 2. Реализовать функции addMetadata (добавление метаданных) и getMetadata (получение метаданных).
+function addMetadata(book, metaDataType, data) {
+    book[metaDataType] = data;
+};
+
+function getMetadata(book, metaDataType) {
+    return book[metaDataType];
+};
+
+//3. Создать объект книги, добавить метаданные и вывести их нa консоль.
+
+const book = {
+    "title": "Война и мир",
+    "author": "Л.Н.Толстой"
+};
+
+addMetadata(book, comment, "Длинная книга с длииииинными предложениями");
+console.log(getMetadata(book, comment));
