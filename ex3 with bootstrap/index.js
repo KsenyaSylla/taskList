1f07d3161aabccf993488e99ba37f7ad0f518334
Sylla Ksenia <natoire@yandex.ru>
2024-02-13 15:59:40 +0300

    redactBtn finished

--- ex3 with bootstrap/index.js
+++ ex3 with bootstrap/index.js
@@ -3,8 +3,6 @@ const startListNumber = 5;
 const container = document.querySelector(".container");
 let lastId = startListNumber;
 const addBtn = document.querySelector(".add");
-const delBtn = document.querySelector(".del");
-const redactBtn = document.querySelector(".redact");
 fetch(url)
   .then(response => response.json())
   .then(result => {
@@ -14,6 +12,8 @@ fetch(url)
 <div class="card-body">
   <h2 class="card-title">${article.title}</h2>
   <p class="card-text">${article.body}</p>
+  <button class="del btn btn-danger" onclick = "delPost(${article.id})">Удалить статью</button>
+  <button class="redact btn btn-warning" onclick = "redactPost(${article.id})">Редактировать стат
 </div>
 </article>`;
       container.insertAdjacentHTML("beforeend", articleHTML);
@@ -21,10 +21,51 @@ fetch(url)
     }
   }); //result - это 100 новостей, которые можно сохранить в local Storage?
 
-/*const article = `<article class="card" style="width: 18rem;" id="${id}">
+addBtn.addEventListener("click", () => {
+  if (container.childElementCount == 0) {
+    fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        let i = 1;
+        let article = result[i];
+        const articleHTML = `<article class="card" id="${article.id}">
 <div class="card-body">
-  <h2 class="card-title">${title}</h2>
-  <p class="card-text">${text}</p>
+  <h2 class="card-title">${article.title}</h2>
+  <p class="card-text">${article.body}</p>
+  <button class="del btn btn-danger" onclick = "delPost(${article.id})">Удалить статью</button>
+  <button class="redact btn btn-warning" onclick = "redactPost(${article.id})">Редактировать стат
 </div>
-</article>`;**/
+</article>`;
+        container.insertAdjacentHTML("beforeend", articleHTML);
+        localStorage.setItem(i, JSON.stringify(article));
+      });
+  } else {
+    fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        let i = ++container.lastChild.id;
+        let article = result[i];
+        const articleHTML = `<article class="card" id="${article.id}">
+<div class="card-body">
+  <h2 class="card-title">${article.title}</h2>
+  <p class="card-text">${article.body}</p>
+  <button class="del btn btn-danger" onclick = "delPost(${article.id})">Удалить статью</button>
+  <button class="redact btn btn-warning" onclick = "redactPost(${article.id})">Редактировать статью</button>
+</div>
+</article>`;
+        container.insertAdjacentHTML("beforeend", articleHTML);
+        localStorage.setItem(i, JSON.stringify(article));
+      })
+  }
+});
 
+function delPost(id) {
+  const articleToDel = document.getElementById(id);
+  articleToDel.remove();
+  localStorage.removeItem(id);
+};
+function redactPost(id) {
+  const articleToRedact = document.getElementById(id);
+  let redactedText = articleToRedact.childNodes[1].childNodes[3];
+  redactedText.textContent = prompt("Введите текст статьи");
+}
